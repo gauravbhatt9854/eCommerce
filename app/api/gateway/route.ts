b6c184fa1bdba8596675922c00e9d9d60f852f73
Gaurav Bhatt <148994632+gauravbhatt9854@users.noreply.github.com>
2025-07-24 13:56:19 +0530

    Update route.ts

--- app/api/gateway/route.ts
+++ app/api/gateway/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_YOUR_KEY_ID as string,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string,
   key_secret: process.env.RAZORPAY_YOUR_SECRET,
 });
 
