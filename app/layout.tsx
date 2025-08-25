b6499997ec33bef177cf6e9739b4640a9c6c8713
Gaurav Bhatt <gbhatt570@gmail.com>
2025-08-26 00:36:14 +0530

    Refactor layout component for improved readability; adjust indentation and formatting for better structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,22 +4,24 @@ import Nav from './(components)/nav/page'
 import { AppStateProvider } from './(components)/provider/AppStateProvider'
 import ChatComponent from './(components)/globalChat/page'
 import { SocketProvider } from "./(components)/provider/SocketProvider";
+import Script from 'next/script'
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <AppStateProvider>
+    <AppStateProvider>
       <SocketProvider>
-          <html lang="en">
-            <body className='relative'>
-              <ChatComponent />
-              <header>
-              </header>
-                <Nav></Nav>
-                <main>{children}</main>
-            </body>
-          </html>
+        <html lang="en">
+          <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}></Script>
+          <body className='relative'>
+            <ChatComponent />
+            <header>
+            </header>
+            <Nav></Nav>
+            <main>{children}</main>
+          </body>
+        </html>
       </SocketProvider>
-      </AppStateProvider>
+    </AppStateProvider>
   )
 }
