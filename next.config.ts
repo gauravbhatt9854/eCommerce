886c99de1496b8fd2c8aecaebdd388c06f4fc3c1
Gaurav Bhatt <gbhatt570@gmail.com>
2025-12-26 17:41:12 +0530

    fix: remove ESLint configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  eslint: {
-    // Option to disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
